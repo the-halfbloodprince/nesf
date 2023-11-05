4643bfdfd3b9589409c0b43a04b98c14d001bb25
Himdyuti1 <himdyutisandilya@kgpian.iitkgp.ac.in>
2023-11-05 11:58:49 +0530

    Added the team.js

--- src/MyComponents/Teams/Governor.js
+++ src/MyComponents/Teams/Governor.js
@@ -1,18 +1,18 @@
 import styles from './Teams.module.css';
 import Card from './Card';
+import Team from './Team.js';
 
 const Governor = () => {
     return ( 
         <div className={` ${styles.governor} ${styles.portfolio}`}>
             <h2>GOVERNORS</h2>
             <div className={styles.items}>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {Team.governors?.map(person=>(
+                    <Card
+                        name={person.name}
+                        image={person.image}
+                    />
+                ))}
             </div>
         </div>
      );
--- src/MyComponents/Teams/President.js
+++ src/MyComponents/Teams/President.js
@@ -1,12 +1,18 @@
 import styles from './Teams.module.css';
 import Card from './Card';
+import Team from './Team.js'
 
 const President = () => {
     return ( 
         <div className={`${styles.president} ${styles.portfolio}`}>
             <h2>PRESIDENT</h2>
             <div className={styles.items}>
-                <Card/>
+                {Team.president?.map(person=>(
+                    <Card
+                        name={person.name}
+                        image={person.image}
+                    />
+                ))}
             </div>
         </div>
      );
