9e5503e1f1223eb316122bb0fe214b13bb8f2930
PenuelDaimari <phd0692@gmail.com>
2024-01-30 00:05:09 +0530

    Updated Articles

--- src/MyComponents/Events/articles.js
+++ src/MyComponents/Events/articles.js
@@ -1,59 +1,23 @@
-import React from "react";
-import styles from "./articles.module.css";
+import React from 'react';
 
-function content() {
-  return (
-    <div>
-      <h1>NORTH-EAST FESTIVAL</h1>
-      {/* Event 1 details */}
-      <p>
-        Hola KGPians! We are really happy to announce that the most awaited
-        North-East Festival 2023 is just a few days away. North-East Festival,
-        the Ceremony of Glamour, the celebration of Culture and the rejoicing of
-        melody filled with the tasteful smell of the traditional delicacies.
-        Witness the ethnic dances from the hills of The North-East and music
-        filled with the mellifluous sound of the Bamboos. Come Get a refined
-        taste of the food made from the astonishing recipe of nature. Observe
-        the culture filled attires and feel the story that the embroidered
-        design is telling us. Let’s Celebrate the festival of culture, the
-        festival of tradition and Ethnicity. Come Let's Celebrate The North-East
-        Festival 2023 on 25th of March 2023 at The Kazi Nazrul Community Centre.
-      </p>
-      {/*Event 2 details*/}
-      <h1>CAREER-COUNSELLING WORKSHOP</h1>
-      <p>
-        We're thrilled to share insights from the Career Counselling Program
-        organized by the Students' Welfare Group on behalf of the North-East
-        Students'Forum, IIT Kharagpur! This impactful initiative aimed to
-        provide the second year northeastern UG students with invaluable
-        guidance and support as they navigate their framed journeys. We hope
-        that they have gained a lot from this session and definitely it will
-        make their forthcoming path smoother.
-      </p>
-    </div>
-  );
+function Articles(){
+    return(
+        <div>
+            <h1>NESF Information</h1>
+            //Event 1 details
+            <p>
+            On August 26th, the North East Students' Forum (NESF) orchestrated a captivating Freshers' Meet, a memorable event dedicated to welcoming the newly arrived North-East freshers.
+The evening transformed into a vibrant tapestry of colors, rich traditions, and exceptional talents and many more.
+We extend our deepest gratitude to all those who graced us with their presence, elevating the NESF Freshers' Meet into a heartwarming celebration of unity and camaraderie. 
+As you venture into this new chapter of your lives, we send our best wishes for a spectacular and fulfilling journey ahead.
+            </p>
+            {/*Event 2 details*/}
+            <p>
+            We're thrilled to share insights from the Career Counselling Program organized by the Students' Welfare Group on behalf of the North-East Students'Forum, IIT Kharagpur!
+This impactful initiative aimed to provide the second year northeastern UG students with invaluable guidance and support as they navigate their framed journeys.
+We hope that they have gained a lot from this session and definitely it will make their forthcoming path smoother.
+            </p>
+        </div>
+    )
 }
-
-// function Page1() {
-//     return (
-//       <div className={styles.green-background}>
-//         <div className={styles.dark-green-square}>Square 1</div>
-//         <div className={styles.dark-green-square}>Square 2</div>
-//       </div>
-//     );
-//   }
-
-//   export default Page1;
-
-//   function Page2() {
-//     return (
-//       <div className={styles.light-violet-background}>
-//         <div className={styles.dark-violet-square}>Square 1</div>
-//         <div className={styles.dark-violet-square}>Square 2</div>
-//       </div>
-//     );
-//   }
-
-//   export default Page2;
-
-export default content;
+export default Articles;
