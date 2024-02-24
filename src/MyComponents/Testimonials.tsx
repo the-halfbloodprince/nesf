50ed53320ebb11a3f9689f22b00e17961ef667e5
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-02-24 18:04:24 +0530

    testimonials

--- src/MyComponents/Testimonials.tsx
+++ src/MyComponents/Testimonials.tsx
@@ -28,12 +28,17 @@ const testimonials: Testimonial[] = [
     name: "Benjamin Doley 3",
     title: "Main maanuh 3"
   },
+  {
+    quote: "I am so excited for the support and guidance I received from NESF. It has helped me grow as a person and as a professional.",
+    name: "Benjamin Doley 4",
+    title: "Main maanuh 4"
+  },
 ]
 
 const Testimonials: FC<Props> = ({ children }) => {
   return (
     <div className={styles.testimonials}>
-        <h2 className={styles.heading}>What people say about us?</h2>
+        <h2 className={styles.heading}>What do people say about us?</h2>
         <InfiniteMovingCards items={testimonials.map(testimonial => <TestimonialCard testimonial={testimonial} />)} />
     </div>
   )
@@ -46,8 +51,10 @@ type TestimonialCardProps = {
 const TestimonialCard: FC<TestimonialCardProps> = ({ testimonial }) => (
   <div className={styles.testimonial_card}>
     <p className={styles.testimonial_card__quote}>{testimonial.quote}</p>
-    <p className={styles.testimonial_card__name}>{testimonial.name}</p>
-    <p className={styles.testimonial_card__title}>{testimonial.title}</p>
+    <div className={styles.testimonial_card__author}>
+      <p className={styles.testimonial_card__name}>{testimonial.name}</p>
+      <p className={styles.testimonial_card__title}>{testimonial.title}</p>
+    </div>
   </div>
 )
 
