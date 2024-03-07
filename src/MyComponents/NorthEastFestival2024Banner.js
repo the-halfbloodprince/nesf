5dbe4b442045b771fa259c2172f805d3bb8f6412
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-03-07 16:03:40 +0530

    updated image nefest24

--- src/MyComponents/NorthEastFestival2024Banner.js
+++ src/MyComponents/NorthEastFestival2024Banner.js
@@ -1,35 +1,38 @@
-import React, { useEffect } from 'react'
-import styles from './NorthEastFestival2024Banner.module.css'
-import Tilt from 'react-parallax-tilt'
-import { SparklesCore } from './AceternitySparkles'
+import React, { useEffect } from 'react';
+import styles from './NorthEastFestival2024Banner.module.css';
+import Tilt from 'react-parallax-tilt';
+import { SparklesCore } from './AceternitySparkles';
 
 const NorthEastFestival2024Banner = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.marqueeText}>
+				{Array.from({ length: 20 }).map((_, index) => (
+					<span>{'Coming Soon ✨'}</span>
+				))}
+			</div>
+			<div className={styles.marqueeText}>
+				{Array.from({ length: 20 }).map((_, index) => (
+					<span>{'Coming Soon ✨'}</span>
+				))}
+			</div>
+			<Tilt className={styles.tiltContainer} glareEnable={true}>
+				<div className={styles.banner}>
+					<img src="/events/northeastfestival20241.png" alt="" />
+					<SparklesCore
+						id="tsparticlesfullpage"
+						background="transparent"
+						minSize={0.6}
+						maxSize={2.4}
+						particleDensity={200}
+						className="absolute w-full h-full"
+						particleColor="#FFFFFF"
+					/>
+					{/* <h1 className={styles.title}>North East Festival <span className={styles.highlighted}>2024</span></h1> */}
+				</div>
+			</Tilt>
+		</div>
+	);
+};
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.marqueeText}>
-        { Array.from({ length: 20 }).map((_, index) => <span>{'Coming Soon ✨'}</span>) }
-      </div>
-      <div className={styles.marqueeText}>
-        { Array.from({ length: 20 }).map((_, index) => <span>{'Coming Soon ✨'}</span>) }
-      </div>
-      <Tilt className={styles.tiltContainer} glareEnable={true}>
-        <div className={styles.banner}>
-          <img src="/events/northeastfestival20241.png" alt="" />
-          <SparklesCore
-            id="tsparticlesfullpage"
-            background="transparent"
-            minSize={0.6}
-            maxSize={2.4}
-            particleDensity={200}
-            className="absolute w-full h-full"
-            particleColor="#FFFFFF"
-           />
-          <h1 className={styles.title}>North East Festival <span className={styles.highlighted}>2024</span></h1>
-        </div>
-      </Tilt>
-    </div>
-  )
-}
-
-export default NorthEastFestival2024Banner
+export default NorthEastFestival2024Banner;
