490dbe866b9c360148c506972dc246daffe7194a
PenuelDaimari <phd0692@gmail.com>
2024-03-16 00:51:45 +0530

    Updated Partnership page

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,38 +1,54 @@
 export const sponsors = [
+  // {
+  //   name: "Assam Tourism",
+  //   image: "assam-tourism.png",
+  // },
   {
-    name: "Assam Tourism",
-    image: "assam-tourism.png",
+    name: "SBI",
+    image: "SBI LOGO.png",
   },
   {
     name: "Arunachal Tourism",
-    image: "arunachal-tourism.png",
+    image: "Arunachal Bamboo Logo.png",
+  },
+  // {
+  //   name: "Manipur Tourism",
+  //   image: "manipur-tourism.png",
+  // },
+  {
+    name: "Manipuris in Kolkata",
+    image: "Manipuris in Kolkata.png",
+  },
+  // {
+  //   name: "Nagaland Tourism",
+  //   image: "nagaland-tourism.png",
+  // },
+  {
+    name: "Mising",
+    image: "Mising Logo.png",
   },
   {
-    name: "Manipur Tourism",
-    image: "manipur-tourism.png",
+    name: "Deep Think AI",
+    image: "Deep Think AI.png",
+  },
+  // {
+  //   name: "Tata Hitachi",
+  //   image: "tata-hitachi.png",
+  // },
+  {
+    name: "M_S DULANG TEA ESTATE",
+    image: "M_S DULANG TEA ESTATE.png",
   },
   {
-    name: "Nagaland Tourism",
-    image: "nagaland-tourism.png",
+    name: "PWC",
+    image: "Poddar Car World.jpeg",
   },
-  {
-    name: "Oil India Limited",
-    image: "oil.png",
-  },
-  {
-    name: "ONGC",
-    image: "ongc.png",
-  },
-  {
-    name: "SBI",
-    image: "sbi.png",
-  },
-  {
-    name: "Tata Hitachi",
-    image: "tata-hitachi.png",
-  },
-  {
-    name: "Untitled",
-    image: "untitled.png",
-  },
+    // {
+  //   name: "Oil India Limited",
+  //   image: "oil.png",
+  // },
+  // {
+  //   name: "ONGC",
+  //   image: "ongc.png",
+  // },
 ]
